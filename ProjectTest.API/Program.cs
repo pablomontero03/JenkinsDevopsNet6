b018b3bced996c23493ad30bc97592df33a5f99d
pablomontero03 <79713314+pablomontero03@users.noreply.github.com>
2022-08-09 20:07:55 -0600

    Update Program.cs

--- ProjectTest.API/Program.cs
+++ ProjectTest.API/Program.cs
@@ -10,13 +10,13 @@ var app = builder.Build();
 app.MapGet("/", () =>
 {
     app.Logger.LogInformation("Initial Home Get");
-    return "GET - Net 6 Running ... 18052022-v1.0.0 CI/CD in TRAVIS-CI v1.0.0 AFORO255 Pablo Montero";
+    return "GET - Net 6 Running ... 18052022-v1.0.0 CI/CD in TRAVIS-CI v1.0.0 AFORO255 Pablo Montero Devops jenkins";
 });
 
 app.MapPost("/", () =>
 {
     app.Logger.LogInformation("Initial Home Post");
-    return "POST - Net 6 Running ... 22042022-v1.0.0 CI/CD in AKS v1.0.0 AFORO255 Pablo Montero";
+    return "POST - Net 6 Running ... 22042022-v1.0.0 CI/CD in AKS v1.0.0 AFORO255 Pablo Montero Devops jenkins";
 });
 
 app.Run();
